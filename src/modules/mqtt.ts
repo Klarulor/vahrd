4ae27fbe58589a5d208a5b5afc574b117ee320b6
Klarulor <klarulor@gmail.com>
2025-03-16 23:43:48 +0200

    rewrite it..

--- src/modules/mqtt.ts
+++ src/modules/mqtt.ts
@@ -81,7 +81,7 @@ function onMessage(topic: string, message: Buffer){
     // home/testvirtualbulb/brightness/set
     // brightness/set
     const routeK = Object.keys(routes).find(x => x == `${target}/${action}`);
-    if(!routeK) return console.log('bad topic', topic);
+    if(!routeK) return console.log('bad topic', topic,'\n',`Allowed: ${Object.keys(routes).join(' ')}`);
     const route = routes[routeK];
     if(route.set)
     {
